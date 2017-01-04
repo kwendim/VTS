650dbb3fedd935a99bc47fb98b47ad94231a100d
kwendim <kwendimagegn@gmail.com>
2017-01-04 15:37:43 +0300

    Initial Commit
    Holds a working login activity

---
+++ app/src/main/java/com/project/groupone/vehicletrackingsystem/AppConfig.java
@@ -0,0 +1,14 @@
+package com.project.groupone.vehicletrackingsystem;
+
+/**
+ * Created by kidus on 1/2/17.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://10.42.0.1/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.42.0.1/android_login_api/register.php";
+
+}
